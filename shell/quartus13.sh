1871d81101ae37cfdcafff52be6a91e36038cd71
Adrià Armejach <adria.armejach@bsc.es>
2025-07-08 11:59:14 +0200

    wayland

--- shell/quartus13.sh
+++ shell/quartus13.sh
@@ -8,20 +8,21 @@ MGLS_LICENSE_FILE=${MGLS_LICENSE_FILE:-"$HOME/.Altera/modelsim.dat"}
 QUARTUS_PATH=/opt/altera/13.0sp1/quartus/linux64
 
 docker run --rm \
---user $(id -u):$(id -g) \
--e DISPLAY \
--e PATH=/bin:/sbin:$QUARTUS_PATH \
--e LD_LIBRARY_PATH=$QUARTUS_PATH \
--e LM_LICENSE_FILE=$LM_LICENSE_FILE \
--e MGLS_LICENSE_FILE=$MGLS_LICENSE_FILE \
---mac-address=$LICENSE_MAC \
--v /etc/passwd:/etc/passwd:ro \
--v /etc/group:/etc/group:ro  \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME:$HOME \
--w $PWD \
---ipc=host \
--v /dev/bus/usb:/dev/bus/usb \
---device-cgroup-rule='c *:* rmw' \
---security-opt seccomp=unconfined \
--ti $QUARTUS13_IMAGE $@
+    --user $(id -u):$(id -g) \
+    -e XDG_RUNTIME_DIR=/tmp \
+    -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+    -e PATH=/bin:/sbin:$QUARTUS_PATH \
+    -e LD_LIBRARY_PATH=$QUARTUS_PATH \
+    -e LM_LICENSE_FILE=$LM_LICENSE_FILE \
+    -e MGLS_LICENSE_FILE=$MGLS_LICENSE_FILE \
+    --mac-address=$LICENSE_MAC \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
+    -v $HOME:$HOME \
+    -w $PWD \
+    --ipc=host \
+    -v /dev/bus/usb:/dev/bus/usb \
+    --device-cgroup-rule='c *:* rmw' \
+    --security-opt seccomp=unconfined \
+    -ti $QUARTUS13_IMAGE $@
